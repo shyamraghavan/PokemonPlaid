292c8b294916157295c2b4530683c6f7fb2aac08
Shyam Raghavan <shyam.raghavan.1@gmail.com>
2013-07-29 11:43:26 -0500

    initial commit, computers problem

---
+++ computers/main.py
@@ -0,0 +1,28 @@
+__author__ = 'shyam'
+
+
+def factorial(num):
+    ret = 1
+    while num > 1:
+        ret = num * ret
+        num -= 1
+    return ret
+
+big_num = factorial(15151)
+sum = 0
+
+while True:
+    x = 2
+    sum = sum + (big_num / (factorial(x) * factorial(15151-x)))
+    x += 2
+    if x == 15150:
+        break
+
+final = ''
+
+print len(str(sum))
+
+for a in xrange(0,len(str(sum))/500):
+    final += str(sum)(a*500)
+
+print final
